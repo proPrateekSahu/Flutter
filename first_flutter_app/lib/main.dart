2021c9862e4766bd641fc35622c638f034fea60a
proPrateekSahu <prateeksahu2706@gmail.com>
2021-05-09 19:42:52 +0530

    1st_flutter_app

---
+++ first_flutter_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      appBar: AppBar(
+        title: Text("My first flutter App"),
+        backgroundColor: Colors.blue,
+      ),
+
+      ),
+    )
+  );
+}
+
+
